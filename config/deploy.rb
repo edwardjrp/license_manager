5bf144872af901f196b5f9ae88f275c40f3d3a06
Edward Rodriguez <edward@blackcubetech.com>
2014-08-29 18:55:55 -0400

    Capistrano deployment recipies added

---
+++ config/deploy.rb
@@ -0,0 +1,86 @@
+# config valid only for Capistrano 3.1
+lock '3.2.1'
+
+set :application, 'alm'
+set :repo_url, 'ssh://edward@inheavenpodcast.capa3.net:8022/home/edward/alm.git'
+
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+
+# Default deploy_to directory is /var/www/my_app
+set :deploy_to, '/home/edward/alm'
+set :keep_releases, 5
+
+# Default value for :scm is :git
+ set :scm, :git
+ #set :user, "edward"
+
+# Default value for :format is :pretty
+ set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, false
+
+#set :use_sudo, false
+
+#set :sudo, "sudo su -u edward -i"
+
+# Default value for :linked_files is []
+# set :linked_files, %w{config/database.yml}
+
+# Default value for linked_dirs is []
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+task :query_interactive do
+  on 'edward@inheavenpodcast.capa3.net:8022' do
+    info capture("[[ $- == *i* ]] && echo 'Interactive' || echo 'Not interactive'")
+  end
+end
+task :query_login do
+  on 'edward@inheavenpodcast.capa3.net:8022' do
+    info capture("shopt -q login_shell && echo 'Login shell' || echo 'Not login shell'")
+  end
+end
+
+task :sudo_test do
+  on 'edward@inheavenpodcast.capa3.net:8022' do
+    info capture("whoami; sudo su - edward")
+  end
+end
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+      #sudo "chmod -R www-data:root alm/"
+      #After deployment deletes development db configuration file
+      execute "rm /home/edward/alm/current/Common.php"
+      #Symlink to the share db configuration files, this is needed because db params are different on dev and prod
+      execute "ln -s /home/edward/alm/shared/Common.php /home/edward/alm/current/Common.php"
+    end
+  end
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+end
---
+++ config/deploy/production.rb
@@ -0,0 +1,51 @@
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary server in each group
+# is considered to be the first unless any hosts have the primary
+# property set.  Don't declare `role :all`, it's a meta role.
+
+role :app, %w{edward@inheavenpodcast.capa3.net}
+#role :web, %w{deploy@example.com}
+#role :db,  %w{deploy@example.com}
+
+
+# Extended Server Syntax
+# ======================
+# This can be used to drop a more detailed server definition into the
+# server list. The second argument is a, or duck-types, Hash and is
+# used to set extended properties on the server.
+
+#set :password, ask('Server password:', nil)
+#server 'inheavenpodcast.capa3.net', user: 'edward', port: 8022, password: fetch(:password)
+server 'inheavenpodcast.capa3.net', user: 'edward', port: 8022
+
+
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
+#
+# Global options
+# --------------
+  set :ssh_options, {
+    keys: %w(/Users/EdwardData/.ssh/id_rsa),
+#    forward_agent: false,
+#    auth_methods: %w(password),
+#    auth_methods: %w(publickey password),
+#    password: "password goes here",
+#    user: "edward",
+#    port: 8022,
+  }
+#
+# And/or per server (overrides global)
+# ------------------------------------
+# server 'example.com',
+#   user: 'user_name',
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: 'user_name', # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: 'please use keys'
+#   }
